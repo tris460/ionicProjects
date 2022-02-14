b5cd30906cb72df2353c20026fd2666cc208d73b
Bety Martinez <trism460@gmail.com>
2022-02-13 18:55:27 -0600

    Add sum()

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class HomePage {
   // Attributes or properties are declared before constructor
   greet: string;
+  addition: number;
   constructor() {
     this.greet = 'Hello world!';
+    this.addition = this.sum(11, 12);
   }
-
+  // Declare functions
+  sum(val1: number, val2: number): number {
+    const total = val1 + val2;
+    return total;
+  }
 }
