b580610f8610d8f2a0179ef867a5acb442ba8ae2
Bety Martinez <trism460@gmail.com>
2022-02-13 19:23:17 -0600

    Create an alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ export class HomePage {
   // Attributes or properties are declared before constructor
   greet: string;
   addition: number;
-  constructor() {
+  constructor(public alert: AlertController) {
     this.greet = 'Hello world!';
     this.addition = this.sum(11, 12);
   }
@@ -17,5 +18,14 @@ export class HomePage {
   sum(val1: number, val2: number): number {
     const total = val1 + val2;
     return total;
+  }
+  async showAlert() {
+    const addAlert = await this.alert.create({
+      header: 'Operation',
+      subHeader: 'The result is:',
+      message: this.addition.toString(),
+      buttons: ['OK']
+    });
+    await addAlert.present();
   }
 }
